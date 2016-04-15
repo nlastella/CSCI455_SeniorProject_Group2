ea55998777fa561b740ad004c39d50326b62aacd
Nicholas Lastella <nlastella88@gmail.com>
2016-04-15 10:48:06 -0400

    Update Enter

--- app/build/intermediates/manifest/androidTest/debug/gen/com/android/csci455_group2/nyit/csci455_seniorproject_group2/test/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.android.csci455_group2.nyit.csci455_seniorproject_group2.test;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
